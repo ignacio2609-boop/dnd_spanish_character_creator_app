6f7b8288e716beb99806fe84a733961880e981a3
Dev_puta_madre <ignacio.bonilla@live.u-tad.com>
2025-11-03 02:25:45 +0100

    Add InteractiveGridPattern and StarsBackground components in HomeView for demo purposes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import PrimeButton from '@/components/prime_vue/PrimeButton.vue'
+import InteractiveGridPattern from '@/components/inspira_ui/InteractiveGridPattern.vue'
+import StarsBackground from '@/components/inspira_ui/StarsBackground.vue'
 </script>
 
 <template>
@@ -10,7 +12,25 @@ import PrimeButton from '@/components/prime_vue/PrimeButton.vue'
       documentation
     </p>
     <PrimeButton />
+    <div class="inspira-theme relative grid h-[500px] place-content-center">
+      <InteractiveGridPattern
+        :class="'[mask-image:radial-gradient(350px_circle_at_center,white,transparent)]'"
+        :width="20"
+        :height="20"
+        :squares="[80, 80]"
+        squares-class-name="hover:fill-blue-500"
+      />
     </div>
+    <div
+      class="inspira-theme relative h-96 w-full border border-zinc-800 rounded-xl overflow-clip flex flex-col items-center justify-center gap-4"
+    >
+      <StarsBackground
+        :factor="0.05"
+        :speed="50"
+        star-color="#fff"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
