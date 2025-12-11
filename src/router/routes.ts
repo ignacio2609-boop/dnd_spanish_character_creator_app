b1e742311532b922844a03a1fc3ee6077c7c0fa1
Dev_puta_madre <ignacio.bonilla@live.u-tad.com>
2025-12-11 01:30:02 +0100

    Add NotFound route placeholder in routes.ts for improved error handling

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,6 +6,14 @@ const routes: RouteRecordRaw[] = [
   { path: '/', name: 'Home', component: Home },
   // Añade más rutas aquí
   { path: '/test-field', name: 'TestField', component: () => import('@/views/TestField.vue') },
+  // ==================================
+  // NOT FOUND - Siempre al final
+  // ==================================
+  // {
+  //   path: '/:catchAll(.*)',
+  //   name: 'NotFound',
+  //   component: () => import('@/views/NotFound.vue'),
+  // },
 ]
 
 export default routes
