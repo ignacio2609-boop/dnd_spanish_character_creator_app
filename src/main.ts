765240d0d0299a953eefe36af9bae628e165986f
Dev_puta_madre <ignacio.bonilla@live.u-tad.com>
2025-12-16 01:59:58 +0100

    lint code

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import '@/styles/main.css'
+import '@/styles/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from '@/router/router.ts'
-import { primeVueConfig } from '@/composables/primeVueConfig.ts'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from '@/router/router.ts';
+import { primeVueConfig } from '@/composables/primeVueConfig.ts';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.use(createPinia())
-primeVueConfig(app)
+app.use(router);
+app.use(createPinia());
+primeVueConfig(app);
 
-app.mount('#app')
+app.mount('#app');
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,19 +1,33 @@
-
-import type { RouteRecordRaw } from 'vue-router'
-import Home from '@/views/HomeView.vue'
+import type { RouteRecordRaw } from 'vue-router';
+import MainLayout from '@/components/layout/MainLayout.vue';
 
 const routes: RouteRecordRaw[] = [
-  { path: '/', name: 'Home', component: Home },
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirect: '/home', // Ruta vacía dentro del layout redirige a /home
+      },
+      {
+        path: 'home',
+        name: 'Home',
+        component: () => import('@/views/HomeView.vue'),
+      },
+      { path: 'test-field', name: 'TestField', component: () => import('@/views/TestField.vue') },
+    ],
+  },
   // Añade más rutas aquí
   { path: '/test-field', name: 'TestField', component: () => import('@/views/TestField.vue') },
   // ==================================
   // NOT FOUND - Siempre al final
   // ==================================
-  // {
-  //   path: '/:catchAll(.*)',
-  //   name: 'NotFound',
-  //   component: () => import('@/views/NotFound.vue'),
-  // },
-]
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: () => import('@/views/NotFound.vue'),
+  },
+];
 
-export default routes
+export default routes;
