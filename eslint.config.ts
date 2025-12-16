87069ccaa15d7046d516e5c2fcfd3027c074d981
Dev_puta_madre <ignacio.bonilla@live.u-tad.com>
2025-12-16 01:58:07 +0100

    update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,22 +1,61 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslint from '@eslint/js';
+import vue from 'eslint-plugin-vue';
+import tseslint from 'typescript-eslint';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
-
-export default defineConfigWithVueTs(
+export default [
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    ignores: [
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      '**/node_modules/**',
+      '*.config.js',
+    ],
   },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
+  eslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  ...tseslint.configs.recommended,
   skipFormatting,
-)
+  {
+    files: ['**/*.{ts,mts,tsx,vue}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        projectService: true,
+      },
+    },
+    rules: {
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          singleline: 10,
+          multiline: { max: 1 },
+        },
+      ],
+      'vue/html-self-closing': 'off',
+      'vue/no-unused-vars': 'warn',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/no-v-html': 'error',
+      'vue/require-prop-types': 'error',
+      'vue/attributes-order': 'error',
+      'vue/multi-word-component-names': 'off',
+
+      'no-console': 'off',
+      'no-restricted-globals': ['error', 'event'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      eqeqeq: 'error',
+      'no-unused-vars': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
