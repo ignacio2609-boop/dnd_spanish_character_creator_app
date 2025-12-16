5e025e6a8e3941c9af83a2ad35c6b6b0ea3037a9
Dev_puta_madre <ignacio.bonilla@live.u-tad.com>
2025-12-16 02:02:42 +0100

    Add support for .vue file extensions in ESLint configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -25,7 +25,8 @@ export default [
         ecmaVersion: 'latest',
         sourceType: 'module',
         projectService: true,
-      },
+        extraFileExtensions: ['.vue']
+      }
     },
     rules: {
       'vue/max-attributes-per-line': [
