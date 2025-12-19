3d590eca337aa5cd76f0f31f6726fe4696dc2d8b
Dev_puta_madre <ignacio.bonilla@live.u-tad.com>
2025-12-19 19:04:13 +0100

    disable drawer

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,26 +1,27 @@
 <script setup lang="ts">
-import PrimeButton from '@/components/prime_vue/PrimeButton.vue';
+// import PrimeButton from '@/components/prime_vue/PrimeButton.vue';
 import mainIcon from '@/assets/main_icon.svg';
 
-const emit = defineEmits(['show-drawer']);
+// const emit = defineEmits(['show-drawer']);
 
-const showDrawer = () => {
-  emit('show-drawer');
-};
+// const showDrawer = () => {
+//   emit('show-drawer');
+// };
 </script>
 
 <template>
   <header>
-    <nav class="relative w-full flex items-center justify-between p-4 lg:px-8 h-16">
-      <PrimeButton
+    <nav class="relative w-full flex items-center justify-center p-4 lg:px-8 h-16">
+      <!-- Botón del drawer deshabilitado temporalmente -->
+      <!-- <PrimeButton
         label="☰"
         text
         rounded
         class="z-10 text-gray-700 !p-0 !w-10 !h-10"
         @click="showDrawer()"
-      />
+      /> -->
 
-      <div class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div class="flex items-center justify-center">
         <a href="/home" class="block">
           <span class="sr-only">Your Company</span>
           <img class="h-12 w-auto" :src="mainIcon" alt="Imagen header" />
--- src/components/layout/MainLayout.vue
+++ src/components/layout/MainLayout.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+// import { ref } from 'vue';
 import Header from '@/components/layout/Header.vue';
-import SideMenu from '@/components/layout/SideMenu.vue';
+// import SideMenu from '@/components/layout/SideMenu.vue'; // Deshabilitado temporalmente
 import Footer from '@/components/layout/Footer.vue';
 
-const isDrawerVisible = ref(false);
+// const isDrawerVisible = ref(false);
 
-const toggleDrawer = () => {
-  isDrawerVisible.value = !isDrawerVisible.value;
-};
+// const toggleDrawer = () => {
+//   isDrawerVisible.value = !isDrawerVisible.value;
+// };
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen relative">
-    <Header @show-drawer="toggleDrawer()" />
+    <Header />
 
-    <SideMenu v-model:visible="isDrawerVisible" />
+    <!-- <SideMenu v-model:visible="isDrawerVisible" /> Deshabilitado temporalmente -->
 
     <main class="flex flex-1 mx-4 my-8">
       <router-view v-slot="{ Component }">
